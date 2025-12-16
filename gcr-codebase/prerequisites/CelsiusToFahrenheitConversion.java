134dd81686bbab6d85e69e1533aabeec8745f945
Krishna Jadhav <krishanraviandrajadhav@gmail.com>
2025-12-16 16:46:39 +0530

    [Krishna Jadhav] : Created a CelsiusToFahrenheitConversion

---
+++ gcr-codebase/prerequisites/CelsiusToFahrenheitConversion.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class CelsiusToFahrenheitConversion{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int celsius = sc.nextInt();
+		int fahrenheit = (celsius * 9/5) + 32;
+		System.out.print(fahrenheit);
+	}
+}
