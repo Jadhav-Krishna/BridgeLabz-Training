72c7519fbd031ffba0b497d213ae4d8058efafd6
Krishna Jadhav <krishanraviandrajadhav@gmail.com>
2025-12-16 20:22:48 +0530

    [Krishna Jadhav] : Created KilometerToMiles convert program

---
+++ gcr-codebase/prerequisites/KilometersToMiles.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class KilometersToMiles{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter kilometers : ");
+		double a = sc.nextDouble();
+		System.out.print(a*0.621371+" miles");
+	}
+}
