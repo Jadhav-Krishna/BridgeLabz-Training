1a76159f4e5202859c94e2296609738b57a99c8b
Krishna Jadhav <krishanraviandrajadhav@gmail.com>
2025-12-17 21:11:47 +0530

    [Krishna Jadhav] : Create UnitPriceOfQuantity to calculate the MaximumNumberOfHandshakes from numberOfStudents

---
+++ gcr-codebase/programming-elements/Level1/MaximumNumberOfHandshakes.java
@@ -0,0 +1,14 @@
+// Initialising the class and the main method
+import java.util.*;
+class MaximumNumberOfHandshakes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // initialising the Variable and calculating Maximum Number Of Handshakes
+        int numberOfStudents = sc.nextInt();
+        int totalMaximumHandshake = (numberOfStudents * (numberOfStudents - 1)) / 2;
+
+        // Displaying the output in the proper format
+        System.out.print("The maximum number of handshakes possible in a group of " + numberOfStudents + " students is " + totalMaximumHandshake);
+    }
+}
