108c67bc9d547fc5f3e9d12918644ac05f0edc87
Krishna Jadhav <krishanraviandrajadhav@gmail.com>
2025-12-17 20:15:21 +0530

    [Krishna Jadhav] : Create Calculator which gives addition, subtraction, multiplication and division

---
+++ gcr-codebase/programming-elements/Level1/Calculator.java
@@ -0,0 +1,18 @@
+// Initialising the class and the main method
+import java.util.*;
+class Calculator{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        // initialising the Variable and calculating the volume of Earth
+        float number1 = sc.nextFloat();
+        float number2 = sc.nextFloat();
+        float addition = number1 + number2;
+        float subtraction = number1 - number2;
+        float multiplication = number1 * number2;
+        float division = number1 / number2;
+
+        // Displaying the output in the proper format
+        System.out.print("The addition, subtraction, multiplication and division value of 2 numbers "+ number1 +" and "+ number2 +" is "+ addition +","+ subtraction +","+ multiplication +",and "+division);
+    }
+}
