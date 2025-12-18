a1c289130758b8275d63edbe87d5230c00ea3edc
Krishna Jadhav <krishanraviandrajadhav@gmail.com>
2025-12-18 12:51:45 +0530

    [Krishna] : Checks weather the number is divisible by 5 or not

---
+++ gcr-codebase/control-flows/Level-1/DivisibleByFive.java
@@ -0,0 +1,29 @@
+// Created a class , and main method with a scanner object
+import java.util.*;
+class DivisibleByFive{
+
+	// Writing the logic to find the number is divisible by 5 or not.
+	public static boolean CheckDivisible(int number){
+		for(int i = 1; i < number ; i++){
+			if(number % 5 == 0){
+				return true;
+			}else{
+				return false;
+			}
+		}
+		return false;
+	}
+
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		// Taking input
+		int number = sc.nextInt();
+		
+		// Function initialisation
+		System.out.print(CheckDivisible(number));
+		
+		// Close the Scanner class
+		sc.close();
+	}
+}
