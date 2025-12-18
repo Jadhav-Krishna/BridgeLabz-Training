335652f044ff714d8ce17fb4c71c2bfe2d941061
Krishna Jadhav <krishanraviandrajadhav@gmail.com>
2025-12-18 10:49:30 +0530

    [Krishna Jadhav] : Created AreaOfTriangle , which calculate the area of a triangle

---
+++ gcr-codebase/programming-elements/Level2/AreaOfTriangle.java
@@ -0,0 +1,15 @@
+// Initialising the class and the main method
+import java.util.*;
+class AreaOfTriangle{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        // initialising the Variable and calculating the Area of Triangle
+        int base = sc.nextInt();
+        int height = sc.nextInt();
+        double area = 1/2 * base * height;
+
+        // Displaying the output in the proper format
+        System.out.print("Height of triangle is "+ height +" while base is "+ base +" and area is "+ area);
+    }
+}
