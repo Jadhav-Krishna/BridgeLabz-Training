0ec29d50bfb5d58d9c37b70b6062a147260a16fb
Krishna Jadhav <krishanraviandrajadhav@gmail.com>
2025-12-19 23:43:03 +0530

    [Krishna] : FizzBuzz , multiple of 3 & 5 . Using While loop

---
+++ gcr-codebase/control-flows/Level-2/FizzBuzzWhile.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+// take a number as user input, and check for a positive integer. If positive integer, loop and print the number, but for multiples of 3 print "Fizz" instead of the number, for multiples of 5 print "Buzz", and for multiples of both print "FizzBuzz".
+public class FizzBuzzWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+
+//      Writing the logic using while loop for multiple of 5 and 3
+        int i = 1;
+        while (i <= number) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+            i++;
+        }
+    }
+}
