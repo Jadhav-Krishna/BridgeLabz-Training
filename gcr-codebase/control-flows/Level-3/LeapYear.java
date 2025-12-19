b8ae34c2bb04928105555d3c15de27bacb526cda
Krishna Jadhav <krishanraviandrajadhav@gmail.com>
2025-12-19 11:14:35 +0530

    [Krishna] : Program to find the year is leap year or not

---
+++ gcr-codebase/control-flows/Level-3/LeapYear.java
@@ -0,0 +1,31 @@
+// Created a class , and 
+import java.util.*;
+class LeapYear{
+
+	// Writing the logic to find the year is leap year or not.
+	// Main method with a scanner object
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		// Taking input
+		System.out.print("Year : ");
+		int year = sc.nextInt();
+		if(year >= 1582){
+			if(year % 100 == 0){
+				if(year % 400 == 0){
+					System.out.print("A Leap year");
+				}
+			}else if(year % 4 == 0){
+				System.out.print("A Leap year");
+			}else{
+				System.out.print("Not a Leap year");
+			}
+			
+		}else{
+			System.out.print("Not a Leap year");
+		}
+		
+		// Close the Scanner class
+		sc.close();
+	}
+}
