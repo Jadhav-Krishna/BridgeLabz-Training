7f5c948cc2b7f546b0f4eea7cc9416b46da984d6
Krishna Jadhav <krishanraviandrajadhav@gmail.com>
2025-12-22 00:31:43 +0530

    [Krishna] : find the roots of a quadratic equation and return the roots.

---
+++ gcr-codebase/methods/Level-2/Quadratic.java
@@ -0,0 +1,35 @@
+import java.util.*;
+//Method to find find the roots of a quadratic equation and return the roots
+public class Quadratic {
+    public static void main() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("a: ");
+        double a = scanner.nextDouble();
+        System.out.print("b: ");
+        double b = scanner.nextDouble();
+        System.out.print("c: ");
+        double c = scanner.nextDouble();
+        double[] roots = findRoots(a, b, c);
+        if (roots.length == 0) {
+            System.out.println("No real roots");
+        } else if (roots.length == 1) {
+            System.out.println("One real root: " + roots[0]);
+        } else {
+            System.out.println("Two real roots: " + roots[0] + " and " + roots[1]);
+        }
+    }
+// Method to find the roots of the quadratic equation ax^2 + bx + c = 0
+    public static double[] findRoots(double a, double b, double c) {
+        double delta = Math.pow(b, 2) - 4 * a * c;
+        if (delta > 0) {
+            double root1 = (-b + Math.sqrt(delta)) / (2 * a);
+            double root2 = (-b - Math.sqrt(delta)) / (2 * a);
+            return new double[]{root1, root2};
+        } else if (delta == 0) {
+            double root = -b / (2 * a);
+            return new double[]{root};
+        } else {
+            return new double[]{};
+        }
+    }
+}
