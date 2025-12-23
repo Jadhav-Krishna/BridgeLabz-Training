91f0d1531fcad17ff99808787d1f7c69dd29d80b
Krishna Jadhav <krishanraviandrajadhav@gmail.com>
2025-12-24 01:41:01 +0530

    [Krishna] : Convert uppercase letters to lowercase and vice versa.

---
+++ gcr-codebase/Extras/Strings/CaseCharacters.java
@@ -0,0 +1,28 @@
+import java.util.*;
+// Convert uppercase letters to lowercase and vice versa.
+
+public class CaseCharacters {
+    public static String toggleCase(String input) {
+        StringBuilder result = new StringBuilder();
+        for (char c : input.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                result.append(Character.toLowerCase(c));
+            } else if (Character.isLowerCase(c)) {
+                result.append(Character.toUpperCase(c));
+            } else {
+                result.append(c);
+            }
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("string: ");
+        String input = scanner.nextLine();
+        String toggledString = toggleCase(input);
+        System.out.println("toggled case: " + toggledString);
+
+        scanner.close();
+    }
+}
