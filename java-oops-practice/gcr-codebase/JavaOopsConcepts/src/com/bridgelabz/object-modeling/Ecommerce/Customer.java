5c87ea16bf5af899f507cd635e1bf22f39f3c334
Krishna Jadhav <krishanraviandrajadhav@gmail.com>
2025-12-31 15:07:48 +0530

    [Krishna] :  e-commerce platform with Order, Customer, and Product classes. Model relationships where a Customer places an Order, and each Order contains multiple Product objects.

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/object-modeling/Ecommerce/Customer.java
@@ -0,0 +1,14 @@
+package Ecommerce;
+
+class Customer {
+    String customerName;
+
+    Customer(String customerName) {
+        this.customerName = customerName;
+    }
+
+    Order placeOrder(int orderId) {
+        System.out.println(customerName + " placed order " + orderId);
+        return new Order(orderId);
+    }
+}
---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/object-modeling/Ecommerce/Product.java
@@ -0,0 +1,11 @@
+package Ecommerce;
+
+public class Product {
+    String productName;
+    double price;
+
+    Product(String productName, double price) {
+        this.productName = productName;
+        this.price = price;
+    }
+}
