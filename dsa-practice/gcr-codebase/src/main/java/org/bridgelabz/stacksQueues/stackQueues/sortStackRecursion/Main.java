f020381e7cdb926d682c44cbd239acbcf2241f67
Krishna Jadhav <krishanraviandrajadhav@gmail.com>
2026-01-05 12:17:37 +0530

    [Krishna] : a stack, sort its elements in ascending order using recursion.

---
+++ dsa-practice/gcr-codebase/src/main/java/org/bridgelabz/stacksQueues/stackQueues/sortStackRecursion/Main.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+class Main{
+    public static void main(String[] args) {
+    	SortStackUsingReccursion sq = new SortStackUsingReccursion();
+        Stack<Integer> st = new Stack<>();
+        st.push(30);
+        st.push(10);
+        st.push(20);
+        st.push(5);
+
+        sq.sortStack(st);
+
+        System.out.println(st);
+    }
+}
---
+++ dsa-practice/gcr-codebase/src/main/java/org/bridgelabz/stacksQueues/stackQueues/sortStackRecursion/SortStackUsingReccursion.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+class SortStackUsingReccursion {
+
+    static void sortStack(Stack<Integer> st) {
+        if (st.isEmpty()) return;
+
+        int top = st.pop();
+        sortStack(st);
+        insertSorted(st, top);
+    }
+
+    static void insertSorted(Stack<Integer> st, int value) {
+        if (st.isEmpty() || st.peek() <= value) {
+            st.push(value);
+            return;
+        }
+        int top = st.pop();
+        insertSorted(st, value);
+        st.push(top);
+    }
+
+}
