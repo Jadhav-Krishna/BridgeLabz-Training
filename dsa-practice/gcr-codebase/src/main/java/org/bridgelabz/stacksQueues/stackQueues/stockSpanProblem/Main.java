3d284711535db7f6b84a96ca4eb76a02d70fb645
Krishna Jadhav <krishanraviandrajadhav@gmail.com>
2026-01-05 12:53:04 +0530

    [Krishna] : For each day in a stock price array, calculate the span (number of consecutive days the price was less than or equal to the current day's price).

---
+++ dsa-practice/gcr-codebase/src/main/java/org/bridgelabz/stacksQueues/stackQueues/stockSpanProblem/Main.java
@@ -0,0 +1,11 @@
+class Main{
+    public static void main(String[] args) {
+        int[] prices = {100, 80, 60, 70, 60, 75, 85};
+        StockSpan sp = new StockSpan();
+        int[] result = sp.calculateSpan(prices);
+
+        for (int x : result) {
+            System.out.print(x + " ");
+        }
+    }
+}
