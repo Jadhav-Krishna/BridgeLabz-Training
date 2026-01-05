87d7add7e65bd199675a9f775f2cba09c992b01c
Krishna Jadhav <krishanraviandrajadhav@gmail.com>
2026-01-05 09:48:01 +0530

    [Krishna] :  online ticket reservation system using a circular linked list, where each node represents a booked ticket.

---
+++ dsa-practice/gcr-codebase/src/main/java/org/bridgelabz/linkedList/ticketReservation/Node.java
@@ -0,0 +1,24 @@
+package linkedlist.ticket_reservation;
+import java.util.Date;
+
+public class Node {
+	
+	int ticketId;
+    String customerName;
+    String movieName;
+    int seatNumber;
+    Date bookingTime;
+
+    Node next;
+
+    public Node(int ticketId, String customerName, String movieName,
+                int seatNumber, Date bookingTime) {
+        this.ticketId = ticketId;
+        this.customerName = customerName;
+        this.movieName = movieName;
+        this.seatNumber = seatNumber;
+        this.bookingTime = bookingTime;
+        this.next = null;
+    }
+
+}
