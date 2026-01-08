73d972ed4b9ae1ceec229daab49044c32897ce56
Krishna Jadhav <krishanraviandrajadhav@gmail.com>
2026-01-08 11:24:46 +0530

    [Krishna] : EventEase helps users organize events like birthdays, weddings, and conferences.

--- java-oops-practice/scenario-based-codebase/src/main/java/org/scenario/EventEase/ISchedulable.java
+++ java-oops-practice/scenario-based-codebase/src/main/java/org/scenario/EventEase/ISchedulable.java
@@ -1,8 +1,8 @@
 package org.scenario.EventEase;
-}
+
+public interface ISchedulable {
+
     void cancel();
     void reschedule(String newDate);
     void schedule();
-public interface ISchedulable {
-
-
+}
