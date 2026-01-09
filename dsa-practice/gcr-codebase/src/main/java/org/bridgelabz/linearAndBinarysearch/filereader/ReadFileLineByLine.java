789667b95f253441e0124b4f44a2860145ae29ab
Krishna Jadhav <krishanraviandrajadhav@gmail.com>
2026-01-10 02:34:56 +0530

    [Krishna] : uses FileReader to read a text file line by line and print each line to the console.

---
+++ dsa-practice/gcr-codebase/src/main/java/org/bridgelabz/linearAndBinarysearch/filereader/ReadFileLineByLine.java
@@ -0,0 +1,19 @@
+package org.bridgelabz.linearAndBinarysearch.filereader;
+
+import java.io.*;
+
+public class ReadFileLineByLine {
+    public static void main(String[] args) throws IOException {
+
+        FileReader fr = new FileReader("sample.txt");
+        BufferedReader br = new BufferedReader(fr);
+
+        String line;
+        while ((line = br.readLine()) != null) {
+            System.out.println(line);
+        }
+
+        br.close();
+        fr.close();
+    }
+}
