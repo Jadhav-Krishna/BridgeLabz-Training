81321a66233a30ca03bf349dd2cd2a622d817303
Krishna Jadhav <krishanraviandrajadhav@gmail.com>
2026-01-15 12:59:34 +0530

    [Krishna] : sort patient beds on the basis of criticality level.

--- dsa-practice/scenario-based-codebase/src/main/java/org/scenario/HospitalQueue/HospitalQueue.java
+++ dsa-practice/scenario-based-codebase/src/main/java/org/scenario/HospitalQueue/HospitalQueue.java
@@ -16,13 +16,18 @@ public class HospitalQueue {
         int n = patients.length;
 
         for (int i = 0; i < n-1; i++) {
+//        	boolean isSwap = false;
             for (int j = 0; j < n-i-1; j++) {
                 if (patients[j].criticality < patients[j + 1].criticality) {
                     Patient temp = patients[j];
                     patients[j] = patients[j + 1];
                     patients[j + 1] = temp;
+//                    isSwap = true;
                 }
             }
+//            if(!isSwap) {
+//            	return;
+//            }
         }
 
         for (Patient p : patients) {
