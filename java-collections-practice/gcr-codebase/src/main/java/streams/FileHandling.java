a9d9846ce7270f762731cf8225927ad383a2228e
Krishna Jadhav <krishanraviandrajadhav@gmail.com>
2026-01-27 04:26:15 +0530

    [Krishna] : reads the contents of a text file and writes it into a new file.

--- java-collections-practice/gcr-codebase/src/main/java/streams/FileHandling.java
+++ java-collections-practice/gcr-codebase/src/main/java/streams/FileHandling.java
@@ -1,13 +1,5 @@
 package streams;
 import java.io.*;
-/* 1. File Handling - Read and Write a Text File
-📌 Problem Statement:
-Write a Java program that reads the contents of a text file and writes it into a new file. If the source file does not exist, display an appropriate message.
-Requirements:
-●	Use FileInputStream and FileOutputStream.
-●	Handle IOException properly.
-●	Ensure that the destination file is created if it does not exist.
- */
 
 public class FileHandling {
 
