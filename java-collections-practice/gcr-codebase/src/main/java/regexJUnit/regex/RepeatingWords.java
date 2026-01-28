ff8be4dfe542b84d86e76eb3d1d351deea4c9914
Krishna Jadhav <krishanraviandrajadhav@gmail.com>
2026-01-28 07:50:49 +0530

    [Krishna] : Find Repeating Words in a Sentence.

---
+++ java-collections-practice/gcr-codebase/src/main/java/regexJUnit/regex/RepeatingWords.java
@@ -0,0 +1,23 @@
+package regexJUnit.regex;
+import java.util.regex.*;
+import java.util.*;
+
+public class RepeatingWords {
+
+	public static void main(String[] args) {
+		String text = "This is is a repeated repeated word test.";
+		String regex = "\\b(\\w+)\\s+\\1\\b";
+		
+		Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+		Matcher matcher = pattern.matcher(text);
+		
+		Set<String> repeatingWords = new HashSet<>();
+		
+		while (matcher.find()) {
+			repeatingWords.add(matcher.group(1));
+		}
+		
+		System.out.println(String.join(", ", repeatingWords));
+	}
+
+}
