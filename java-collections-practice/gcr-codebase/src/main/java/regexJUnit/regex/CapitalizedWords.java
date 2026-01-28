b0c0380577d885c9ee47fdaeec3b704ee7d945fe
Krishna Jadhav <krishanraviandrajadhav@gmail.com>
2026-01-28 07:44:27 +0530

    [Krishna] : Extract All Capitalized Words from a Sentence.

---
+++ java-collections-practice/gcr-codebase/src/main/java/regexJUnit/regex/CapitalizedWords.java
@@ -0,0 +1,22 @@
+package regexJUnit.regex;
+import java.util.*;
+import java.util.regex.*;
+
+public class CapitalizedWords {
+
+	public static void main(String[] args) {
+		String text = "The Eiffel Tower is in Paris and the Statue of Liberty is in New York.";
+		String regex = "\\b[A-Z][a-zA-Z]*\\b";
+
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(text);
+
+		List<String> capitalizedWords = new ArrayList<>();
+
+		while (matcher.find()) {
+			capitalizedWords.add(matcher.group());
+		}
+
+		System.out.println(String.join(", ", capitalizedWords));
+	}
+}
