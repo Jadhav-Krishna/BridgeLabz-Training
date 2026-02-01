3cde6c61026032f9fea6db5c6f5a1fb5658d6b95
Krishna Jadhav <krishanraviandrajadhav@gmail.com>
2026-02-02 03:52:17 +0530

    [Krishna] : forEach method with a lambda expression to send notification emails to all users.

---
+++ java8-programming-practice/gcr-codebase/src/main/java/streamAPI/EmailNotifications.java
@@ -0,0 +1,18 @@
+package streamAPI;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class EmailNotifications {
+
+    public static void main(String[] args) {
+
+        List<String> emails = Arrays.asList("user1@gmail.com","user2@gmail.com","user3@gmail.com");
+
+        emails.forEach(email -> sendEmailNotification(email));
+    }
+
+    private static void sendEmailNotification(String email) {
+        System.out.println("Notification sent to: " + email);
+    }
+}
