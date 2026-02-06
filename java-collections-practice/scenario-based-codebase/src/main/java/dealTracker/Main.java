d7e9ab393fada8202608add3700365bf249f0bb1
Krishna Jadhav <krishanraviandrajadhav@gmail.com>
2026-02-06 10:46:28 +0530

    [Krishna] : e-commerce platform is flooded with daily deals, sent in a file

---
+++ java-collections-practice/scenario-based-codebase/src/main/java/dealTracker/Main.java
@@ -0,0 +1,68 @@
+package dealTracker;
+
+import dealTracker.DealTrackerManager;
+
+import java.util.Scanner;
+
+public class Main {
+
+	private static final String DEFAULT_DEAL_FOLDER = "D:\\BridgeLabz-Training\\java-collections-practice\\scenario-based-codebase\\src\\main\\java\\dealTracker";
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        DealTrackerManager<Deal> manager = new DealTrackerManager<>();
+
+        int choice = -1;
+
+        System.out.println("Welcome to DealTracker");
+
+        while (choice != 0) {
+            System.out.println("\nSelect an option:");
+            System.out.println("1 Load Deals from Default Folder");
+            System.out.println("2 Display All Valid Deals");
+            System.out.println("3 Display Deals Sorted by Discount");
+            System.out.println("4 Exit");
+
+            System.out.print("Enter choice: ");
+            String input = sc.nextLine().trim();
+
+            if (input.isEmpty()) {
+                System.out.println(" Please enter a number (0-3)!");
+                continue;
+            }
+
+            try {
+                choice = Integer.parseInt(input);
+            } catch (NumberFormatException e) {
+                System.out.println(" Invalid input! Enter a number between 0-3");
+                continue;
+            }
+
+            switch (choice) {
+                case 1:
+                    System.out.println(" Loading deals from default folder...");
+                    manager.loadDealsFromFolder(DEFAULT_DEAL_FOLDER);
+                    break;
+
+                case 2:
+                    manager.displayAllDeals();
+                    break;
+
+                case 3:
+                    manager.displayDealsSortedByDiscount();
+                    break;
+
+                case 0:
+                    System.out.println(" Exiting DealTracker. Goodbye!");
+                    break;
+
+                default:
+                    System.out.println(" Invalid choice! Enter a number between 0-3");
+            }
+        }
+
+        sc.close();
+    }
+}
