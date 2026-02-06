26d05351faaf98d939310f66db33080e8bfbf0a5
Krishna Jadhav <krishanraviandrajadhav@gmail.com>
2026-02-06 10:38:32 +0530

    [Krishna] :  school system uploads student marks as .csv files: RollNo,Name,Subject,Marks

---
+++ java-collections-practice/scenario-based-codebase/src/main/java/examResultUploader/InvalidMarkFormatException.java
@@ -0,0 +1,7 @@
+package examResultUploader;
+
+public class InvalidMarkFormatException extends Exception {
+    public InvalidMarkFormatException(String message) {
+        super(message);
+    }
+}
